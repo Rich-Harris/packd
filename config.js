49caaaaff024f3500d2eaff9bc8eca95d039405d
Sven Sauleau <sven@cloudflare.com>
2020-05-02 21:14:44 +0100

    configuration bundle.run

--- config.js
+++ config.js
@@ -22,15 +22,16 @@ exports.additionalBundleResHeaders = {
 };
 
 if (process.env.BUNDLE_RUN) {
+	exports.tmpdir = "/tmp";
 	const cacheExpiration = 60 * 60 * 24 * 365;
-	exports.npmInstallEnvVars = ["npm_config_cache=~/.npm"];
+	exports.npmInstallEnvVars = ["npm_config_cache=/tmp"];
 	exports.debugEndpoints = false;
 	exports.additionalBundleResHeaders = {
 		'Cache-Control': 'public, max-age=' + cacheExpiration,
 		'X-Content-Type-Options': 'nosniff',
 		'Access-Control-Allow-Origin': '*',
 		'Access-Control-Request-Method': 'GET',
-		'X-Powered-By': 'https://github.com/rich-harris/packd',
+		'X-Powered-By': 'github.com/rich-harris/packd',
 		'Strict-Transport-Security': `max-age=${cacheExpiration}; includeSubDomains; preload`,
 	};
 }
